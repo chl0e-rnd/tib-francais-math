c633029949f545abc69edb4b1577af101c2acdef
Tobias Glauser <tobiasglauser2006@gmail.com>
2025-05-10 13:16:08 +0200

    Application fonctionnelle

--- src/App.vue
+++ src/App.vue
@@ -21,16 +21,18 @@ function calculateLegibilityText() {
     alert('Veuillez entrer un texte avant de calculer la lisibilité.')
     return
   }
-
+  console.log(1)
   // Effectue les algoritmes sur les tests
-  gunningResult.value = getGunningResult(text.value);
+  getGunningResult(text.value).then((res) => {
+    gunningResult.value = res
+  })
+  console.log(2)
   //left 55px pour 0, 470px pour fin -> 64 diff | 415 px de long, 415 / 64 = 25,90
   //top 287px pour 0, 17px pour fin -> 11,74
   getFryResult(text.value).then((v) => {
     fryResult.value = v
 
     setPointCoordonates(v[0], v[1]);
-    // setPointCoordonates(128, 5)
   });
 }
 
